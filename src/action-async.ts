128d618558475893e84fbb21b95fa6cb86011845
Carlos <ckelkboom@gmail.com>
2016-07-05 10:49:36 +0200

    We now have composable async Actions

--- src/action-async.ts
+++ src/action-async.ts
@@ -2,37 +2,67 @@
 import { IAction, IFunctor, StepFn } from './interfaces';
 import { Promise } from 'es6-promise';
 
-export class AsyncAction<T> implements IAction<Promise<T>> {
+function promisify(val:any) {
+    if (val instanceof Promise) return val;
+    else {
+        return new Promise(function (resolve) {
+            setTimeout(() => resolve(val));
+        });
+    }
+}
+
+export class AsyncAction<T> implements IAction<Promise<T>>, IFunctor {
 
     private step:StepFn<T>;
+    private length:number;
 
- 
+
     /*
      The constructor function taking a Step Function as a parameter
      The step function is of type: (...args:any[]) => T
      */
-    constructor(f:StepFn<T>) {
+    constructor(f:StepFn<T>, n?:number) {
         this.step = f;
+        this.length = n || f.length;
     }
 
-    ap(...args) {
+    ap(...args):Promise<T> | AsyncAction<Promise<T>> {
+
         // of course we'd want to support currying...
-        if (args.length < this.step.length) {
+        if (args.length < this.length) {
             let curried = (...args2) => this.ap.apply(this, args.concat(args2));
             return new AsyncAction(curried) as AsyncAction<Promise<T>>;
         }
-        
         else {
-            let result = this.step.apply(null, args);
-            if (result instanceof Promise) {
-                return result
-            }
-            else {
-                return new Promise(resolve => {
-                   setTimeout(() => resolve(result));
-                });
-            }
+            return promisify(this.step.apply(this, args)) as Promise<T>;
         }
+    }
+
+
+    map<U>(t:StepFn<U>) {
+
+        // create the new function by composing the current step with the new function
+        // f(g(x)) so we do something like:
+        // t( this.step(x) );
+        let f:StepFn<U> = (...args) => {
+
+            return new Promise((resolve) => {
+                this.ap.apply(this, args).then(r1 => {
+
+                    var r2 = t(r1);
+                    if (r2 instanceof Promise) {
+                        r2.then(r3 => resolve(r3));
+                    }
+                    else {
+                        resolve(r2);
+                    }
+
+                });
+            });
+
+        };
+
+        return new AsyncAction(f, this.length);
     }
 
 }
--- tests/async-action.spec.ts
+++ tests/async-action.spec.ts
@@ -1,16 +1,70 @@
 /// <reference path="./../typings/index.d.ts" />
 
 import { AsyncAction } from './../src/action-async';
+import { Promise } from 'es6-promise';
 
 export default function () {
 
     describe('An Action async', () => {
         it('exists', () => expect(AsyncAction).toBeDefined());
-        it('can be fired', async function(done) {
+
+        it('works with promises', async function(done) {
+            let doubleMe = function(n) {
+                return new Promise(resolve => {
+                    setTimeout(() => resolve(n * 2));
+                });
+            };
+
+            let a = new AsyncAction(doubleMe);
+            (a.ap(6) as Promise<number>).then(r => {
+                done(expect(r).toEqual(12));
+            });
+        });
+
+        it('can supports currying', async function(done) {
             let add:(x:number, y:number) => number = (x,y) => x + y;
             let a = new AsyncAction(add);
-            let r = await a.ap(12).ap(13);
+            let r = await (a.ap(12) as AsyncAction<Promise<number>>).ap(13);
             done(expect(r).toEqual(25));
+        });
+
+        it('is composable', async function(done) {
+            let add:(x:number, y:number) => number = (x,y) => x + y;
+            let doubleMe = function(n) {
+                return new Promise(resolve => {
+                    setTimeout(() => resolve(n * 2));
+                });
+            };
+
+            let a = new AsyncAction(add);
+            let b = a.map(doubleMe);
+            let c = b.ap(12, 13) as Promise<number>;
+            c.then(r => {
+
+                done(expect(r).toEqual(50));
+
+            });
+
+        });
+
+        it('is composable with currying', async function(done) {
+            let add:(x:number, y:number) => number = (x,y) => x + y;
+            let doubleMe = function(n) {
+                return new Promise(resolve => {
+                    setTimeout(() => resolve(n * 2));
+                });
+            };
+
+            let a = new AsyncAction(add);
+            let b = a.map(doubleMe);
+            let c = b.ap(12) as AsyncAction;
+            let d = c.ap(13) as Promise<number>;
+            d.then(r => {
+
+                done(expect(r).toEqual(50));
+
+            });
+
         });
     });
 }
